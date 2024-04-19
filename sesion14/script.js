aa290487455dfb8949994aaaf173621d53f2b154
jhon1253 <sebastianquinayas@gmail.com>
2024-04-19 12:42:53 -0500

    eliminar carpeta

--- sesion14/script.js
+++
@@ -1,9 +0,0 @@
-var visitas = localStorage.getItem("visitas");
-if (visitas === null) {
-    visitas = 0
-} else {
-    visitas = parseInt(visitas);
-}
-visitas = visitas +1 ;
-console.log("Numero de visitas", visitas);
-localStorage.setItem("visitas", visitas)
