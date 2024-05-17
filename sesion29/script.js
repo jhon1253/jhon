9799edd4ae2d00aced3f9bab81c269e8b128aafe
jhon1253 <sebastianquinayas@gmail.com>
2024-05-17 12:02:15 -0500

    sesion 45

--- sesion29/script.js
+++ sesion29/script.js
@@ -1,7 +1,7 @@
-Ejercicio 1: Dibujar un Cuadro de Asteriscos
-Crea una función en JavaScript que reciba un número entero n y dibuje
- un cuadro de n x n asteriscos. Por ejemplo, si n es 4, debería
-Imprimir:
+// Ejercicio 1: Dibujar un Cuadro de Asteriscos
+// Crea una función en JavaScript que reciba un número entero n y dibuje
+//  un cuadro de n x n asteriscos. Por ejemplo, si n es 4, debería
+// Imprimir:
 
 function dibujarCuadroAsteriscos(numero) {
     
@@ -19,9 +19,9 @@ function dibujarCuadroAsteriscos(numero) {
 
 dibujarCuadroAsteriscos(4)
 
-Ejercicio 2: Generar Patrones
-Crear un patrón usando bucles anidados. Por ejemplo, 
-generar un triángulo rectángulo con asteriscos.
+// Ejercicio 2: Generar Patrones
+// Crear un patrón usando bucles anidados. Por ejemplo, 
+// generar un triángulo rectángulo con asteriscos.
 
 
 function generarTrianguloRectangulo(filas) {
@@ -36,10 +36,10 @@ function generarTrianguloRectangulo(filas) {
     
     generarTrianguloRectangulo(8);
 
-    Ejercicio 3: Serie de Fibonacci hasta n, 
-    Escribe una función que genere la serie de Fibonacci hastaun número n dado.
-     La serie de Fibonacci es una secuencia donde cada número es la suma de los dos números anteriores,
-      empezando con 0 y 1. Por ejemplo, si n es 10, debería imprimir:
+    // Ejercicio 3: Serie de Fibonacci hasta n, 
+    // Escribe una función que genere la serie de Fibonacci hastaun número n dado.
+    //  La serie de Fibonacci es una secuencia donde cada número es la suma de los dos números anteriores,
+    //   empezando con 0 y 1. Por ejemplo, si n es 10, debería imprimir:
 
 let numero = prompt("ingrese numero: ")
 function fib(numeros){
