95cfc0d6b855c8d3e631c5c1357df9ddaff7b8f7
jhon1253 <sebastianquinayas@gmail.com>
2024-07-30 15:32:01 -0500

    cambios

--- sesion89/ComponentsList/src/App.jsx
+++ sesion89/ComponentsList/src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import ItemCard from './Components/ItemCard/index';
+import ContainerHouses from './Components/ContainerHouses/ContainerHouses';
+import WrapperYellow from './components/WrapperYellow/WrapperYellow';
+import Table from './components/Table/Table';
+import TableRow from './components/Table/TableRow';
+import TableHeader from './components/Table/TableHeader';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <ContainerHouses />
+        <Table
+          render={(item) => (
+            <TableRow>
+              <div>{item.name}</div>
+              <div>{item.price}</div>
+            </TableRow>
+          )}
+          columns={["Nombre", "Precio"]}
+          columnsRender={(column) => <TableHeader>{column}</TableHeader>}
+          data={[
+            { name: "Casa 1", price: 100000 },
+            { name: "Casa 2", price: 200000 },
+          ]}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
