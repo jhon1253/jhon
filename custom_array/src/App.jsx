195d662220d80f9d2b1fc7bd5372f91806afd8c6
jhon1253 <sebastianquinayas@gmail.com>
2024-07-31 14:26:19 -0500

    custom

--- custom_array/src/App.jsx
+++ custom_array/src/App.jsx
@@ -1,17 +1,30 @@
-import { useState } from 'react'
-import './App.css'
-import NotificationMessage from './Componets/NotificationMessage'
+import { useState } from "react";
+import "./App.css";
+import { BrowserRouter, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import { Route } from "react-router-dom";
+import About from "./pages/About";
+import CategoryProducts from "./pages/CategoryProducts";
+import NotificationMessage from "./Componets/NotificationMessage";
+import Categories from "./pages/Categories";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <NotificationMessage/>
-      <h6>puede existir mas contenido</h6>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/categories" element={<Categories />}></Route>
+          <Route
+            path="/categoryproducts"
+            element={<CategoryProducts />}
+          ></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
 
-export default App
+export default App;
---
+++ custom_array/src/pages/CategoryProducts.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+
+ const CategoryProducts = () => {
+  return (
+    <div>CategoryProducts</div>
+  )
+}
+export default CategoryProducts
